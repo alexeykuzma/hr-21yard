e874321db483386982e5de8cb42428dd937ae6d3
Aleksey Kuzma <alexey.kuzma@outlook.com>
2024-05-11 02:35:12 +0500

    Update SubList.jsx

--- src/components/GroupList/SubList/SubList.jsx
+++ src/components/GroupList/SubList/SubList.jsx
@@ -11,30 +11,21 @@ const SubList = (props) =>{
 
   list = list.concat(item.map(el => {
     return <li key={el.id}
-    // style={{ display: "flex", width: 100 }}
     >
-      {/* {el.id} {el.name} {el.groups.length ? <span onClick={() => {props.click(el.groups)}}>NEXT</span> : null} */}
       <ListItem item={el} clickNext={clickNext}/>
     </li>
   }))
 
   return(
     <ul className={style.SubList}>
-      {/* <button onClick={clickBack}>back</button> */}
       {(backName!=='')?<li><BackItem name={backName} clickBack={clickBack} /></li>:null}
-      {/* <li>test назад</li> */}
       
       {Array.isArray(item) ? item.map(el => {
         return <li key={el.id}
-        // style={{ display: "flex", width: 100 }}
         >
-          {/* {el.id} {el.name} {el.groups.length ? <span onClick={() => {props.click(el.groups)}}>NEXT</span> : null} */}
           <ListItem item={el} clickNext={clickNext}/>
         </li>
       }):null}
-
-      {/* {list}  */}
-    
     </ul>
 
   )
